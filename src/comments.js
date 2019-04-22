8b78aa5ee7c6a3742748fde040bb08331358c169
dev_johsire <josiah5115@gmail.com>
2019-04-22 17:53:13 -0600

    Change Square Comp from Class to Function Comp

--- src/comments.js
+++ src/comments.js
@@ -1,15 +1,15 @@
 /*
-        Why Immutability is Important
+        !Why Immutability is Important
 
-    In the previous code example, we suggested using the .slice() operator to create a copy
+    -In the previous code example, we suggested using the .slice() operator to create a copy
     of the squares array to modify instead of modifying the existing array.
-    We’ll now discuss immutability and why immutability is important to learn.
+    -We’ll now discuss immutability and why immutability is important to learn.
 
-    There are generally 2 approaches to changing data.
-    1) To mutate the data by directly changing the data’s values.
-    2) To replace the data with a new copy which has the desired changes.
+    -There are generally 2 approaches to changing data.
+    *1) To mutate the data by directly changing the data’s values.
+    *2) To replace the data with a new copy which has the desired changes.
 
-    1) Data Change with Mutation
+    !1) Data Change with Mutation
 
         var player = {score: 1, name: 'Jeff'};
         player.score = 2;
@@ -17,37 +17,51 @@
         Now player is {score: 2, name: 'Jeff'}
 
 
-    2) Data Change without Mutation
+    !2) Data Change without Mutation
 
         var player = {score: 1, name: 'Stanley'};
         var newPlayer = Object.assign({}, player, {score: 2});
 
-        Now player is unchanged, but newPlayer is {score: 2, name: 'Jeff'}
+        -Now player is unchanged, but newPlayer is {score: 2, name: 'Jeff'}
 
-    Or if you are using object Spread Syntax Proposal, you can write:
+    *Or if you are using object Spread Syntax Proposal, you can write:
         var newPlayer = {...player, score: 2};
 
-    The end result is the same but by not mutating (or changing the underlying data) directly,
+    -The end result is the same but by not mutating (or changing the underlying data) directly,
     we gain several benefits described below.
 
-    1) Complex Features Become Simple
-    Immutability makes complex features much easier to implement. Later in this tutorial, we will implement
+   * 1) Complex Features Become Simple
+    -Immutability makes complex features much easier to implement. Later in this tutorial, we will implement
     a “time travel” feature that allows us to review the tic-tac-toe game’s history & “jump back” to previous moves.
-    This functionality isn’t specific to games — ability to undo & redo certain actions is a common requirement in apps.
-    Avoiding direct data mutation lets us keep previous versions of the game’s history intact, & reuse them later.
+    -This functionality isn’t specific to games — ability to undo & redo certain actions is a common requirement in apps.
+    -Avoiding direct data mutation lets us keep previous versions of the game’s history intact, & reuse them later.
 
-    2) Detecting Changes
-    Detecting changes in mutable objs is difficult because they are modified directly.
-    This detection requires the mutable obj to be compared to previous copies of itself & the entire object tree traversed.
+   * 2) Detecting Changes
+    -Detecting changes in mutable objs is difficult because they are modified directly.
+    -This detection requires the mutable obj to be compared to previous copies of itself & the entire object tree traversed.
 
-    Detecting changes in immutable objs is considerably easier. If the immutable obj that is being referenced is different
+    -Detecting changes in immutable objs is considerably easier. If the immutable obj that is being referenced is different
     than the previous one, then the obj has changed.
 
-    3) Determining When to Re-Render in React
-    The main benefit of immutability is that it helps build 'pure components' in React. Immutable data can easily determine
+   * 3) Determining When to Re-Render in React
+    -The main benefit of immutability is that it helps build 'pure components' in React. Immutable data can easily determine
     if changes have been made which helps to determine when a component requires re-rendering.
 
-    Learn more about shouldComponentUpdate() & how to build pure components by reading Optimizing Performance.
-    https://reactjs.org/docs/optimizing-performance.html#examples
+    *Learn more about shouldComponentUpdate() & how to build pure components by reading Optimizing Performance.
+    *https://reactjs.org/docs/optimizing-performance.html#examples
+
+ */
+
+/**
+ *              !Function Component
+ *
+ ** We'll now change the Square to be a Function Component
+
+* In React, fn comps are a simpler way to write comps that only contain a render method & don’t have their own state.
+
+* Instead of defining a class which extends React.Component, we can write a fn that takes props as input &
+returns what should be rendered.
+
+* Fn comps are less tedious to write than classes, & many components can be expressed this way.
 
  */
--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 // Square Component
-class Square extends React.Component {
+//class Square extends React.Component {
 
-  /* We use constroctor to initialize state in our Components.
-     Add 'this.state' in our 'constractor method'.
+  /*  We use constructor to initialize state in our Components.
+      Add 'this.state' in our 'constructor method'.
      this.state is private to the component its defined in. */
 
   // constructor(props){
 
     /*In JS classes, you need to always call super when defining
-     the constructor of a subclass. All React component classes that have
-     a constructor should start it with a super(props) call.*/
+      the constructor of a subclass. All React component classes that have
+      a constructor should start it with a super(props) call.*/
 
     // super(props);
 
@@ -40,35 +40,67 @@ class Square extends React.Component {
     method, & the code would work the same. In React, it’s conventional to use on[Event] names for props which represent events
     & handle[Event] for the methods which handle the events. */
 
-  render() {
-    return (
-     /* if we forget to write pass the arrow fn () => it will result in the alert being each time the components re-renders.
-        This is a common mistake.*/
+  // render() {
+  //   return (
+     /**
+      * !if we forget to write pass the arrow fn () => it will result in the alert being each time the components re-renders.
+      ** This is a common mistake
+      */
 
-      <button
-        className="square"
-        /* By calling 'this.setState' from an onClick handler in the Square's render mthd,
-          we tell React to re-render that Square everytime its '<button>' is clicked.
-         After the update, the Square's this.state.value will be set to 'X' */
+      /* <button
 
-        onClick={() => this.props.onClick({ value: 'X' })}
+        className="square" */
+
+        /**
+         **By calling 'this.setState' from an onClick handler in the Square's render method,
+          **we tell React to re-render that Square every time its '<button>' is clicked.
+         **After the update, the Square's this.state.value will be set to 'X'
+         */
+
+        // onClick={() => this.props.onClick({ value: 'X' })}
         /*When you call setState in a component, React automatically updates the child components inside of it too.*/
-      >
-      {/* Now we’re passing down two props from Board to Square: 'value' & 'onClick'.
-      The onClick prop is a fn that Square can call when clicked. */}
+      // >
+      /* Now we’re passing down two props from Board to Square: 'value' & 'onClick'.
+      The onClick prop is a fn that Square can call when clicked. */
 
-        {this.props.value}
+//         {this.props.value}
+//       </button>
+//     );
+//   };
+// };
+
+
+/**
+ * !Change our Square Component from a Class to Function Component since it only
+ * !contains a render method & has no state to maintain
+
+  **We also changed this.props to props on both times it appears.
+ */
+
+  function Square(props) {
+    return (
+      <button className="square" onClick={ props.onClick }>
+        { props.value }
       </button>
     );
   };
-};
+
+  /**
+   * !Note
+   * When we modified the Square to be a function component, we also changed
+   * *onClick={() => this.props.onClick()} to a shorter onClick={props.onClick}
+   * !(note the lack of parentheses on both sides).
+   */
+
+
+
 
 // Board Component
 class Board extends React.Component {
 
   /*Currently, each Square component maintains the game’s state. To check for a winner, we’ll
     maintain the value of each of the 9 squares in one location - Lifting State Up to parent
-    Board Comp instead of in each Sqare- child. Board(parent) will then tell each Sqare(Child)
+    Board Comp instead of in each Square- child. Board(parent) will then tell each Square(Child)
     what to display by passing a prop.*/
 
   constructor(props) {
@@ -78,15 +110,15 @@ class Board extends React.Component {
       };
   }
 
-  // Our handleClick Custom method- it can have any name & code would still work the same;
+  // *Our handleClick is a Custom method- it can have any name & code would still work the same;
 
-  /* Since the Square components no longer maintain state, the Square components receive values
-     from the Board component and inform the Board component when they’re clicked. In React terms,
-     the Square components are now controlled components. The Board has full control over them. */
+  /*  Since the Square components no longer maintain state, the Square components receive values
+      from the Board component and inform the Board component when they’re clicked. In React terms,
+      the Square components are now controlled components. The Board has full control over them. */
 
   handleClick(i) {
 
-    /* We call .slice() on handleClick to creat a clpy of the sqares array to modify instead of
+    /* We call .slice() on handleClick to creat a copy of the squares array to modify instead of
       modifying the existing array bcoz of Immutability */
 
     const squares = this.state.squares.slice();
