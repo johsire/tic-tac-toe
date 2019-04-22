9bcc007014921963b677e05442b01a4969b6f87e
dev_johsire <josiah5115@gmail.com>
2019-04-22 04:19:54 -0600

    Passing down props from Board to Square: Value & OnClick

--- src/index.js
+++ src/index.js
@@ -5,35 +5,45 @@ import './index.css';
 
 // Square Component
 class Square extends React.Component {
-  // We use constroctor to initialize state in our Components.
-  // Add 'this.state' in our 'constractor method'.
-  // this.state is private to the component its defined in.
-  constructor(props){
+  /* We use constroctor to initialize state in our Components.
+     Add 'this.state' in our 'constractor method'.
+     this.state is private to the component its defined in. */
+
+  // constructor(props){
+
     /*In JS classes, you need to always call super when defining
      the constructor of a subclass. All React component classes that have
      a constructor should start it with a super(props) call.*/
-    super(props);
-    // Comp use state to 'remember' things.
-    // In our case we want our comp to rem it was clicked on.
-    this.state = {
+
+    // super(props);
+
+    /* Components use state to 'remember' things.
+       In our case we want our component to rem it was clicked on. */
+
+    /* this.state = {
       value: null,
     };
-  }
+  } */
 
   render() {
     return (
-      // if we forget to write pass the arrow fn () => it will result in the alert being
-      // each time the components re-renders.This is a common mistake.
+     /* if we forget to write pass the arrow fn () => it will result in the alert being
+      each time the components re-renders.This is a common mistake.*/
+
       <button
         className="square"
         /* By calling 'this.setState' from an onClick handler in the Square's render mthd,
           we tell React to re-render that Square everytime its '<button>' is clicked.
          After the update, the Square's this.state.value will be set to 'X' */
-        onClick={() => this.setState({ value: 'X' })}
+
+        onClick={() => this.props.onClick({ value: 'X' })}
         /* When you call setState in a component, React automatically updates the child
          components inside of it too. */
       >
-        {this.state.value}
+      {/* Now we’re passing down two props from Board to Square: 'value' & 'onClick'.
+      The onClick prop is a fn that Square can call when clicked. */}
+
+        {this.props.value}
       </button>
     );
   };
@@ -58,8 +68,19 @@ class Board extends React.Component {
 
   renderSquare(i) {
     /*Each Square will now receive a value prop that will either be 'X', 'O', or null for empty squares.*/
-    return <Square value={this.state.squares[i]} />;
-  }
+    return (
+      // added parentheses so that JS doesn’t insert a semicolon after return and break our code.
+
+      /* We need to change what happens when a Square is clicked. The Board component now maintains which squares
+      are filled. We need to create a way for the Square to update the Board’s state. Since state is considered to
+      be private to a component that defines it, we cannot update the Board’s state directly from Square. */
+      <Square
+        value={this.state.squares[i]}
+        /* we’ll pass down a fn from the Board to the Square, have Square call that fn when a square is clicked.*/
+        onClick={() => this.handleClick(i)}
+        />
+    );
+  };
 
   render() {
     const status = 'Next player: X';
